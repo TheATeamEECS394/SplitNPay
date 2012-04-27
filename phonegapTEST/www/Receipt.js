4dedd05d6a81b4cad184714c941286df1726b072
Benjamin English <BenjaminEnglish2013@u.northwestern.edu>
2012-04-26 20:25:54 -0500

    Added quantity and headers to table

--- phonegapTEST/www/Receipt.js
+++ phonegapTEST/www/Receipt.js
@@ -1,9 +1,8 @@
-<<<<<<< HEAD
 function Receipt(arrayOfitems){
 this.items = arrayOfitems;
 
 Receipt.prototype.toString = function(){
-	var tableStart = "<table border=1>";
+	var tableStart = "<table border=1><tr><th>Quantity</th><th>Description</th><th>Price</th><th>Payees</th></tr>";
 	var tableBody = "";
 	var tableEnd = "</table>";
 
@@ -16,29 +15,34 @@ Receipt.prototype.toString = function(){
 	}
 }
 
-function receiptItem(des,p){
+function receiptItem(qty,des,p){
+this.quantity = qty;
 this.description = des;
 this.price = p;
 this.payees = new Array;
+this.payees.push("Generic Payee");
+
+this.AddPayee = function(){
+	var NewPayee = prompt("Enter the new Payees name:","Your name here");
+	this.payees.push(NewPayee);
+	alert("Payees include: "+this.payees);
+}
 
 receiptItem.prototype.toString = function(){
-var rowStart = "<tr>";
-var rowEnd = "</tr>"
-var rowBody = "";
-var rowString = "<tr onClick=" +
-				"\"MyAlert(this);\"> <td>" + this.description +"</td>" +
-				"<td>" + this.price + "</td> </tr>";
+var rowString = "<tr> <td>" + this.quantity + "</td>" +
+				"<td>" + this.description +"</td>" +
+				"<td>" + this.price + "</td>" + 
+				"<td>" + this.payees + "</td>" + "</tr>";
 				return rowString;
-			
 			}
 			
 }
 
 var payees = new Array();
 
-var item1 = new receiptItem("shrimp",30);
-var item2 = new receiptItem("steak",40);
-var item3 = new receiptItem("asparagus",10);
+var item1 = new receiptItem(1,"shrimp",30);
+var item2 = new receiptItem(2,"steak",40);
+var item3 = new receiptItem(1,"asparagus",10);
 
 var receipt1 = new Array();
 receipt1[0] = item1;
@@ -47,53 +51,3 @@ receipt1[2] = item3;
 
 var m = new Receipt(receipt1);
 
-=======
-function Receipt(arrayOfitems){
-this.items = arrayOfitems;
-
-Receipt.prototype.toString = function(){
-	var tableStart = "<table border=1>";
-	var tableBody = "";
-	var tableEnd = "</table>";
-
-	for(i=0;i<this.items.length;i++)
-	{
-		tableBody = tableBody + this.items[i];
-	}
-
-	return tableStart + tableBody + tableEnd;
-	}
-}
-
-function receiptItem(des,p){
-this.description = des;
-this.price = p;
-this.payees = new Array;
-
-receiptItem.prototype.toString = function(){
-var rowStart = "<tr>";
-var rowEnd = "</tr>"
-var rowBody = "";
-var rowString = "<tr onClick=" +
-				"\"MyAlert(this);\"> <td>" + this.description +"</td>" +
-				"<td>" + this.price + "</td> </tr>";
-				return rowString;
-			
-			}
-			
-}
-
-var payees = new Array();
-
-var item1 = new receiptItem("shrimp",30);
-var item2 = new receiptItem("steak",40);
-var item3 = new receiptItem("asparagus",10);
-
-var receipt1 = new Array();
-receipt1[0] = item1;
-receipt1[1] = item2;
-receipt1[2] = item3;
-
-var m = new Receipt(receipt1);
-
->>>>>>> 39af9ea2be6afcdcf7fd53178bad4414c86abc3e
